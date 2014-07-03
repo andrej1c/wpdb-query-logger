86681a479ac4907baa8417fa9a7e148f21fbdb64
Andrej Ciho <Andrej@lappy486.(none)>
2014-07-03 11:54:58 -0400

    Formatting the php code better and adding .gitignore for the logs

--- wpdb-query-logger.php
+++ wpdb-query-logger.php
@@ -8,29 +8,35 @@
  */
 
 
-class CS_WPDB_Logger
-{
-	function log($query)
-	{
+class AC_WPDB_Logger {
+	function log( $query ) {
 		// Do not log queries that only SELECT
 		$pos1 = strpos( $query, 'INSERT'  );
 		$pos2 = strpos( $query, 'UPDATE'  );
 		$pos3 = strpos( $query, 'REPLACE' );
-		if (($pos1 === false) && ($pos2 === false) && ($pos3 === false)) return $query;
+		if ( ( $pos1 === false ) && ( $pos2 === false ) && ( $pos3 === false ) ) {
+			return $query;
+		}
 
 		// Grab username
 		global $current_user;
-		$user_name = empty($current_user) ? 'notloggedin' : $current_user->user_login;
+		$user_name = empty( $current_user ) ? 'notloggedin' : $current_user->user_login;
 		
 		// Open File and Write
-		$filename = dirname(__FILE__). '/logs/wpdb_queries-'.date('Y-m-d').'-'.$user_name.'.sql';
-		if (!$handle = @fopen($filename, 'a')) return $query;
-		@fwrite($handle, $query . "; --".date('H:i:s')."\n");
-		@fclose($handle);
+		$filename = dirname( __FILE__) . '/logs/wpdb_queries-' . date( 'Y-m-d' ) . '-' . $user_name . '.sql';
+		if ( ! $handle = @fopen( $filename, 'a' ) ) {
+			return $query;
+		}
+		@fwrite( $handle, $query . "; --" . date( 'H:i:s' ) . "\n" );
+		@fclose( $handle );
 		
 		// This is a filter so we have to return $query
 		return $query;
 	}
 }
 
-if (defined('LOG_QUERIES')) if (true == LOG_QUERIES) add_filter('query', array('CS_WPDB_Logger', 'log'),1,1);
+if ( defined( 'WP_DEBUG_LOG' ) ) {
+	if ( true == WP_DEBUG_LOG ) {
+		add_filter( 'query', array( 'AC_WPDB_Logger', 'log' ) , 1, 1 );
+	}
+}
